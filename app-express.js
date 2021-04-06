16a0c82c5ccbe9d1fe9896d789cdea4a93658549
MJCC <reception.mjcc@laposte.net>
2021-04-06 16:12:58 +0200

    Exercice pour gitworkflow et feuille route pour le rebase

--- app-express.js
+++ app-express.js
@@ -2,12 +2,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const panier1 = require('./panier/panier1.json')
 
 app.get('/welcome', (req, res) => {
   res.send('Bienvenue !')
 });
 
 app.use('/equipe', express.static('equipe'));
+
+app.get('/panier', (req, res) => {
+  res.json(panier1);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
